be4c6dbc859566abe661e54aada89ac2776f2363
Hob Spillane <hob.spillane@workday.com>
2011-12-07 12:40:16 -0500

    Applied hanging quotation marks

--- assets/js/testimonials.js
+++ assets/js/testimonials.js
@@ -1,7 +1,7 @@
 $(document).ready(ready);
 function ready()
 {
-	var testimonials = [{quote:'Brian and his crew do a fantastic job.  Like most of Brians\'s clients, we are repeat customers largely because of his ability to understand what we are looking for.  I thought I knew what I wanted when we started our kitchen renovation but Brian\'s suggestions and ideas really improved on our original design.  His staff produces quality work with a real focus on details.  We look forward to our next project with Spillane Custom Carpentry.', author:'Emilia Pietrangelo', authorLocation:'Williams Twp, PA'}];
+	var testimonials = [{quote:'"Brian and his crew do a fantastic job.  Like most of Brians\'s clients, we are repeat customers largely because of his ability to understand what we are looking for.  I thought I knew what I wanted when we started our kitchen renovation but Brian\'s suggestions and ideas really improved on our original design.  His staff produces quality work with a real focus on details.  We look forward to our next project with Spillane Custom Carpentry."', author:'Emilia Pietrangelo', authorLocation:'Williams Twp, PA'}];
 	var randomIndex = Math.floor(Math.random() * testimonials.length);
 	var testimonial = testimonials[randomIndex];
 	$('#testimonial').html(testimonial.quote);
