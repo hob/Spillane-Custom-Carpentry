890acc6b32d3fb4f7de01a690e75befb0f1fece8
Hob Spillane <hob.spillane@workday.com>
2012-08-30 14:40:40 +0100

    Adding simple http server so I can run it locally & test the facebook social integration without posting to production

---
+++ simpleHttpServer.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+	
+	var filePath = '.' + request.url;
+	if(filePath == './')
+	{
+		filePath = './index.html';
+	}
+	var extName = path.extname(filePath);
+	var contType = 'text/html';
+	switch(extName) {
+		case '.js':
+			contType = 'text/javascript';
+			break;
+		case '.css':
+			contType = 'text/css';
+			break;
+		case '.png':
+			contType = 'image/png';
+			break;
+	}
+
+	fs.exists(filePath, function(exists) {
+		if(exists) {
+			fs.readFile(filePath, function(error, content){
+				if(error) {
+					response.writeHead(500);
+					response.end();
+				}else{
+					response.writeHead(200, {'Content-Type':contType});
+					response.end(content, 'utf-8');
+				}
+			});
+		}else{
+			response.writeHead(404);
+			response.end();
+		}
+	});
+	
+}).listen(8125);
+console.log('Server is running at http://127.0.0.1:8125/');
